b100c8748d516c6b9942fd53cac36e420fe80635
stijn <stijn@users.noreply.github.com>
2024-05-30 13:10:01 +0200

    hello

--- server/server.js
+++ server/server.js
@@ -1,28 +1,29 @@
 const express = require('express');
 const app = express();
+const { Server } = require('http');
 const path = require('path');
 const http = require('http').Server(app);
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const dbURI = 'mongodb+srv://stijnsepter:a8S3ijniJB3ELTxn@cbroefenexamen.ksykwxl.mongodb.net/?retryWrites=true&w=majority&appName=CBRoefenexamen';
 let db = undefined;
 
 app.use(express.static('client'))
 const hostname = 'localhost';
-
 const port = process.env.PORT||8181;
 
 //attach http server to the socket io
-const io = require('socket.io')(http);
+const io = new Server(app);
 
 http.listen(port, hostname, ()=>{
     console.log(`Server running at http://${hostname}:${port}`)
 })
 
-let kiosksConnected = 0;
+let Connected = 0;
 io.on('connection', socket =>{
-    kiosksConnected++
-
+    Connected++
+    console.log('Connected to' + Connected);
     socket.on('disconnect', ()=>{
-        kiosksConnected--
-        console.log('kiosk disconnected');
+        Connected--
+        console.log('CBR_examen disconnected');
     })
 });
